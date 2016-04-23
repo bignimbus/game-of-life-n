f712d0fcd90c6a3ccc2bbc2a6aa7f2194e8cd0dc
bignimbus <jdauriemma@gmail.com>
2016-04-23 12:46:58 -0500

    Initial test specs for grid

--- grid.js
+++ grid.js
@@ -1,75 +1,90 @@
+/*
+ * The grid we use is a 1-dimensional array.  Using arithmetic via the transform function, 
+ * this array can represent a square, cubic, or hypercubic plane of arbitrary size.
+ * Ex: a cube (l = 3) would be represented by an array of length 27 (3 ^ 3).
+ * Array[9] would represent the coordinate (0, 0, 1)
+ */
+
 'use strict';
 
 // jumps {unit} units across {dimension} axis from point {coord}
-const transform = (coord, dimension, unit) => {
-  let shift = dimension === 0 && !unit ? 0 : Math.pow(global.SIZE, dimension);
-  return coord + (unit || 0) * shift;
+// ex: in a representation of a cube (l = 3): transform(0, 1, 1) means to return the point
+// from (0, 0, 0) that is 1 unit away across the second (y) axis.  transform(0, 1, 1) === 3,
+// which can be expressed as coordinate (0, 1, 0)
+const transform = (point, ax, multiple) => {
+  let shift = ax === 0 && !multiple ? 0 : Math.pow(global.SIZE, ax);
+  return point + (multiple || 0) * shift;
 };
 
 const inBounds = (num) => {
-  return num < Math.pow(global.SIZE, global.DIMENSIONS) && num >= 0;
+  return num < global.PLANE_SIZE && num >= 0;
 };
 
-// iterates across all existing points on the
-// {dimension}-axis in the line intersecting point {coord}
-const axis = function * (coord, dimension) {
-  for (let n = global.SIZE * -1; n < global.SIZE; n++) {
-    let c = transform(coord, dimension, n);
-    if (inBounds(c)) yield c;
+// returns the point that intersects with the origin of {ax} axis
+const getAxisOrigin = (point, ax) => {
+  for (let n = -global.SIZE + 1; n < global.SIZE; n++) {
+    let c = transform(point, ax, n);
+    if (c % Math.pow(global.SIZE, ax + 1) < Math.pow(global.SIZE, ax)) return n;
   }
 };
 
-
-// iterates over all existing points
-// within {bound} units of point {coord} across
-// {dimension}-axis
-const segmentIterator = function * (dimension, bound, coord) {
-  for (let n = 0 - bound; n <= bound; n++) {
-    yield transform(coord, dimension, n);
+// returns an array of all points that may exist on the
+// {ax}-axis in the line intersecting {point}
+const axis = (point, ax) => {
+  let arr = [];
+  for (let n = getAxisOrigin(point, ax); n < global.SIZE; n++) {
+    let c = transform(point, ax, n);
+    if (inBounds(c)) arr.push(c);
+    if (arr.length === global.SIZE) break;
   }
+  return arr;
 };
 
-const segment = function * (dimension, bound, coord) {
-  let a = [...axis(coord, dimension)],
-    s = [...segmentIterator(dimension, bound, coord)];
-  for (let n of s.filter((num) => a.indexOf(num) !== -1)) {
-    yield n;
+const possibleSegment = function (ax, point, upperBound, lowerBound) {
+  let arr = [];
+  for (let n = lowerBound; n <= upperBound; n++) {
+    arr.push(transform(point, ax, n));
   }
+  return arr;
+};
+
+// slices all common elements between segmentIterator and axis
+// to return a segment of points that definitely exist
+// within {bound} of {point} along {ax} axis
+const segment = function (ax, point, upperBound, lowerBound) {
+  lowerBound = lowerBound || -upperBound;
+  let a = axis(point, ax),
+    s = possibleSegment(ax, point, upperBound, lowerBound),
+    arr = [];
+  s.forEach((num, i) => {if (a.indexOf(num) !== -1) arr.push([num, i - 1])});
+  return arr;
 };
 
 // returns all the points within {bound} points
 // of {coord} on all axes
-const region = function * (bound, coord, dimension) {
+const region = function * (coord, dimension) {
   if (dimension === global.DIMENSIONS) {
-    if (inBounds(coord)) yield coord;
+    yield coord;
     return;
   }
-  for (let n of segment(dimension, bound, coord)) {
-    yield * region(bound, n, dimension + 1);
+  for (let n of segment(dimension, coord, 1, -1)) {
+    yield * region(n[0], dimension + 1);
   }
 };
 
 // do not return the same coordinate as the origin
-const gate = function * (iterator, bounds, coord) {
-  for (let n of iterator(bounds, coord, 0)) {
+const gate = function * (iterator, coord) {
+  for (let n of iterator(coord, 0)) {
     if (n !== coord) yield n;
   }
 };
 
-// for each point in the grid, return an iterator for its neighbors
-const grid = function * (plane) {
-  let iterator = gate.bind(null, region, 1);
-  for (let n of plane) {
-    yield iterator(n);
-  }
-};
-
 module.exports = {
-  grid: grid,
   gate: gate,
-  region: gate.bind(null, region, 1),
+  region: gate.bind(null, region),
   segment: segment,
-  segmentIterator: segmentIterator,
+  possibleSegment: possibleSegment,
+  getAxisOrigin: getAxisOrigin,
   axis: axis,
   inBounds: inBounds,
   transform: transform
---
+++ spec/grid.spec.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const grid = require('../grid');
+
+const stubGrid = (obj) => {
+  obj = obj || {};
+  global.SIZE = obj.size || 4;
+  global.DIMENSIONS = obj.dimensions || 4;
+  global.PLANE_SIZE = Math.pow(global.SIZE, global.DIMENSIONS);
+};
+
+describe('grid functions', () => {
+  it('should be imported as an object', () => {
+    expect(typeof grid).toBe('object');
+  });
+
+  describe('transform', () => {
+    let transform = grid.transform;
+    it('given a point, axis, and multiple, returns the array index that represents a transformation across {axis} {multiple} points away from {point}', () => {
+      stubGrid();
+      expect(transform(0, 0, 0)).toBe(0);
+      expect(transform(0, 1, 0)).toBe(0);
+      expect(transform(0, 1, 1)).toBe(4);
+      expect(transform(0, 2, 1)).toBe(16);
+      expect(transform(0, 3, 1)).toBe(64);
+      expect(transform(0, 3, -1)).toBe(-64);
+      expect(transform(1, 3, -1)).toBe(-63);
+    });
+  });
+
+  describe('inBounds', () => {
+    let inBounds = grid.inBounds;
+    it('given a point, returns a boolean indicating whether that point exists on the plane', () => {
+      stubGrid();
+      expect(inBounds(0)).toBe(true);
+      expect(inBounds(-1)).toBe(false);
+      expect(inBounds(255)).toBe(true);
+      expect(inBounds(256)).toBe(false);
+    });
+  });
+
+  describe('getAxisOrigin', () => {
+    let getAxisOrigin = grid.getAxisOrigin;
+    it('finds the point that forms a line segment with {point} that intersects with the origin of {ax} axis', () => {
+      stubGrid();
+    });
+  });
+
+  describe('axis', () => {
+    let axis = grid.axis;
+    it('given an axis and point, iterates over all points on that axis that could exist and intersect with {point}', () => {
+      stubGrid();
+      expect([...axis(0, 0)]).toEqual([0, 1, 2, 3]);
+      expect([...axis(0, 1)]).toEqual([0, 4, 8, 12]);
+      expect([...axis(0, 2)]).toEqual([0, 16, 32, 48]);
+      expect([...axis(0, 3)]).toEqual([0, 64, 128, 192]);
+      expect([...axis(1, 3)]).toEqual([1, 65, 129, 193]);
+      expect([...axis(3, 0)]).toEqual([0, 1, 2, 3]);
+      expect([...axis(192, 0)]).toEqual([192, 193, 194, 195]);
+      expect([...axis(192, 1)]).toEqual([192, 196, 200, 204]);
+      expect([...axis(255, 0)]).toEqual([252, 253, 254, 255]);
+    });
+  });
+
+  describe('possibleSegment', () => {
+    let possibleSegment = grid.possibleSegment;
+    it('given an axis, point and bound, iterates over all existing or non-existant points on that axis within {bound} units of {point}', () => {
+      stubGrid();
+      expect(possibleSegment(0, 1, 1, -1)).toEqual([0, 1, 2]);
+      expect(possibleSegment(0, 3, 1, -1)).toEqual([2, 3, 4]);
+      expect(possibleSegment(0, 3, 2, -2)).toEqual([1, 2, 3, 4, 5]);
+      expect(possibleSegment(1, 0, 1, -1)).toEqual([-4, 0, 4]);
+      expect(possibleSegment(0, 255, 1, -1)).toEqual([254, 255, 256]);
+      expect(possibleSegment(0, 4, 1, -1)).toEqual([3, 4, 5]);
+    });
+  });
+
+  describe('segment', () => {
+    let segment = grid.segment;
+    it('given an axis, point and bound, returns all existing points on that axis within {bound} units of {point} and the corresponding index of the line segment', () => {
+      stubGrid();
+      expect(segment(0, 0, 1, -1)).toEqual([[0, 0], [1, 1]]);
+      expect(segment(0, 1, 1, -1)).toEqual([[0, -1], [1, 0], [2, 1]]);
+      expect(segment(0, 3, 1, -1)).toEqual([[2, -1], [3, 0]]);
+      expect(segment(0, 3, 2, -2)).toEqual([[1, -1], [2, 0], [3, 1]]);
+      expect(segment(1, 0, 1, -1)).toEqual([[0, 0], [4, 1]]);
+      expect(segment(0, 255, 1, -1)).toEqual([[254, -1], [255, 0]]);
+      expect(segment(0, 4, 1, -1)).toEqual([[4, 0], [5, 1]]);
+    });
+  });
+
+  describe('gate', () => {
+    let gate = grid.gate;
+    function * testIterator () {
+      for (let n = 0; n < 4; n++) {
+        yield n;
+      }
+    }
+    it('given an iterator and a point, returns an iterator without that point', () => {
+      expect([...testIterator()]).toEqual([0, 1, 2, 3]);
+      expect([...gate(testIterator, 1)]).toEqual([0, 2, 3]);
+    });
+  });
+
+  describe('region', () => {
+    let region = grid.region;
+    it('given a bound and a point, iterates over all points {bound} or fewer units away', () => {
+      stubGrid({size: 4, dimensions: 2});
+      expect([...region(0)].sort((a, b) => a > b ? 1 : -1)).toEqual([1, 4, 5]);
+      expect([...region(1)].sort((a, b) => a > b ? 1 : -1)).toEqual([0, 2, 4, 5, 6]);
+      expect([...region(2)].sort((a, b) => a > b ? 1 : -1)).toEqual([1, 3, 5, 6, 7]);
+      expect([...region(3)].sort((a, b) => a > b ? 1 : -1)).toEqual([2, 6, 7]);
+      expect([...region(5)].sort((a, b) => a > b ? 1 : -1)).toEqual([0, 1, 2, 4, 6, 8, 9, 10]);
+
+      stubGrid({size: 3, dimensions: 3});
+      expect([...region(0)].sort((a, b) => a > b ? 1 : -1)).toEqual([1, 3, 4, 9, 10, 12, 13]);
+      expect([...region(13)].sort((a, b) => a > b ? 1 : -1)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+      expect([...region(26)].sort((a, b) => a > b ? 1 : -1)).toEqual([13, 14, 16, 17, 22, 23, 25]);
+
+      stubGrid({size: 4, dimensions: 4});
+      expect([...region(0)].sort((a, b) => a > b ? 1 : -1)).toEqual([1, 4, 5, 16, 17, 20, 21, 64, 65, 68, 69, 80, 81, 84, 85]);
+      expect([...region(149)].sort((a, b) => a > b ? 1 : -1)).toEqual([
+        64, 65, 66, 68, 69, 70, 72, 73, 74,
+        80, 81, 82, 84, 85, 86, 88, 89, 90,
+        96, 97, 98, 100, 101, 102, 104, 105, 106,
+        128, 129, 130, 132, 133, 134, 136, 137, 138,
+        144, 145, 146, 148, 150, 152, 153, 154,
+        160, 161, 162, 164, 165, 166, 168, 169, 170,
+        192, 193, 194, 196, 197, 198, 200, 201, 202,
+        208, 209, 210, 212, 213, 214, 216, 217, 218,
+        224, 225, 226, 228, 229, 230, 232, 233, 234
+      ]);
+    });
+  });
+});
